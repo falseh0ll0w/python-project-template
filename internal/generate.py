83c158bf32f69f83482c3209dc371b12f5208d44
h0_0w <146596580+falseh0ll0w@users.noreply.github.com>
2023-11-07 01:58:18 -0800

    1

--- internal/generate.py
+++ internal/generate.py
@@ -16,25 +16,26 @@ import subprocess
 
 
 ## Python 2.6 subprocess.check_output compatibility. Thanks Greg Hewgill!
-if 'check_output' not in dir(subprocess):
+if "check_output" not in dir(subprocess):
+
     def check_output(cmd_args, *args, **kwargs):
         proc = subprocess.Popen(
-            cmd_args, *args,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+            cmd_args, *args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs
+        )
         out, err = proc.communicate()
         if proc.returncode != 0:
             raise subprocess.CalledProcessError(args)
         return out
+
     subprocess.check_output = check_output
 
 
 def main():
     # If not in the project root directory, go to it.
-    project_root = os.path.dirname(os.path.dirname(
-        os.path.realpath(__file__)))
+    project_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     os.chdir(project_root)
 
-    sys.path.insert(0, os.path.realpath('my_module'))
+    sys.path.insert(0, os.path.realpath("my_module"))
     # Can't use `from my_module import metadata' since `__init__.py' is
     # templated.
     import metadata
@@ -42,20 +43,20 @@ def main():
     # The title of the main documentation should match the project name in the
     # metadata. Under it should be a matching underline of equal signs. Produce
     # it for the template.
-    project_underline = len(metadata.project) * '='
+    project_underline = len(metadata.project) * "="
 
     # Substitute values into template files and produce their real
     # counterparts.
-    for dirpath, dirnames, filenames in os.walk('.'):
+    for dirpath, dirnames, filenames in os.walk("."):
         # Don't recurse into git directory.
-        if '.git' in dirnames:
-            dirnames.remove('.git')
+        if ".git" in dirnames:
+            dirnames.remove(".git")
 
         for filename in filenames:
             # Ignore all non-template files.
             # Using splitext cuts off the last extension.
             root, extension = os.path.splitext(filename)
-            if extension != '.tpl':
+            if extension != ".tpl":
                 continue
 
             # Substitute values and write the new file.
@@ -64,45 +65,47 @@ def main():
 
             with open(tpl_path) as tpl_file:
                 template = Template(tpl_file.read())
-            print('Substituting', tpl_path, '->', real_path)
-            with open(real_path, 'w') as real_file:
-                real_file.write(template.safe_substitute(
-                    project_underline=project_underline,
-                    **metadata.__dict__))
+            print("Substituting", tpl_path, "->", real_path)
+            with open(real_path, "w") as real_file:
+                real_file.write(
+                    template.safe_substitute(
+                        project_underline=project_underline, **metadata.__dict__
+                    )
+                )
 
             # Remove the template file.
             os.remove(tpl_path)
 
-    print('Renaming the package: my_module ->', metadata.package)
-    os.rename('my_module', metadata.package)
+    print("Renaming the package: my_module ->", metadata.package)
+    os.rename("my_module", metadata.package)
 
-    print('Making main script executable...')
-    main_py_path = os.path.join(metadata.package, 'main.py')
+    print("Making main script executable...")
+    main_py_path = os.path.join(metadata.package, "main.py")
     # This is a no-op on Windows.
     os.chmod(main_py_path, os.stat(main_py_path).st_mode | stat.S_IXUSR)
 
-    print('Removing internal Travis-CI test file...')
-    os.remove('.travis.yml')
+    print("Removing internal Travis-CI test file...")
+    os.remove(".travis.yml")
 
     print("Revising `LICENSE' file...")
     # Open file for reading and writing.
-    with open('LICENSE', 'r+') as license_file:
+    with open("LICENSE", "r+") as license_file:
         # Strip off the first two lines.
-        new_contents = ''.join(license_file.readlines()[2:])
+        new_contents = "".join(license_file.readlines()[2:])
         license_file.seek(0)
         print(new_contents, file=license_file)
 
-    print('Removing internal directory...')
-    shutil.rmtree('internal')
+    print("Removing internal directory...")
+    shutil.rmtree("internal")
 
-    git_revision = subprocess.check_output(
-        ['git', 'rev-parse', 'HEAD']).rstrip()
-    print('Dropping PPT version cookie (revision {0})...'.format(git_revision))
+    git_revision = subprocess.check_output(["git", "rev-parse", "HEAD"]).rstrip()
+    print("Dropping PPT version cookie (revision {0})...".format(git_revision))
     # Append the commit hash of the current revision to the file.
-    with open('.ppt-version', 'a') as ppt_version_cookie_file:
+    with open(".ppt-version", "a") as ppt_version_cookie_file:
         print(git_revision, file=ppt_version_cookie_file)
 
-    print('''
+    print(
+        """
 To finish project setup:
 
 1. Change the `classifiers' keyword in `setup.py' as necessary.
@@ -111,8 +114,9 @@ To finish project setup:
    change is necessary.
 3. Install `argparse' package when developing on Python 2.6.
 4. Change `README.rst' to your own text.
-''')
+"""
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
